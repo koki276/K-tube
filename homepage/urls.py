729ef9810248a8ff9a6d24cd457f354717094d88
LAPTOP-16ET0KVR\inoue koki <seaton1605.211@softbank.ne.jp>
2018-06-28 10:11:39 +0900

    modified css and html

--- homepage/urls.py
+++ homepage/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.top_page, name='top_page'),
+    path('members/', views.members, name='members'),
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def top_page(request):
     return render(request, 'homepage/top_page.html', {})
+
+def members(request):
+    return render(request, 'homepage/members.html', {})
